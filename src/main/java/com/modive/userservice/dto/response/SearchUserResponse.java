735e87e68e12a1f085e56cfb6d23995a3bad5cf9
SRASONY <leeseula0123@gmail.com>
2025-06-05 15:14:54 +0900

    test(#10): Lombok 어노테이션 추가
    @NoArgsConstructor, @AllArgsConstructor추가

--- src/main/java/com/modive/userservice/dto/response/SearchUserResponse.java
+++ src/main/java/com/modive/userservice/dto/response/SearchUserResponse.java
@@ -2,10 +2,12 @@ package com.modive.userservice.dto.response;
 import com.modive.userservice.domain.UserInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SearchUserResponse {
     private List<UserInfo> searchResult;
